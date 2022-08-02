f3995ac3b687cc4e4c5b2c317a05a54bd8432b79
Mohammed F A <mohammedfazalabdulla@gmail.com>
2022-08-02 12:48:08 +0530

    hello

---
+++ tutorial1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Text('Hello!'),
+    );
+  }
+}
