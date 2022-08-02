7a606bf2814843b7d145b8ea21314ce222786439
Mohammed F A <mohammedfazalabdulla@gmail.com>
2022-08-02 13:04:39 +0530

    build body and appbar

--- tutorial1/lib/main.dart
+++ tutorial1/lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Hello!'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tutorial 1'),
+        ),
+        body: Text('Hi Guys!!!!'),
+      ),
     );
   }
 }
