32a35ba6d580fbdb7ee1869ef329846c11019e39
Mohammed F A <mohammedfazalabdulla@gmail.com>
2022-08-02 13:36:36 +0530

    implement buuton

--- tutorial1/lib/main.dart
+++ tutorial1/lib/main.dart
@@ -9,14 +9,32 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  void answerQuestion() {
+    print('answer 1 chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['What is your name?', 'Where are you from?'];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Tutorial 1'),
+          title: const Text('Tutorial 1'),
         ),
-        body: Text('Hi Guys!!!!'),
+        body: Column(
+          children: [
+            const Text('The questions:'),
+            ElevatedButton(onPressed: answerQuestion, child: Text('Answer 1')),
+            ElevatedButton(
+                onPressed: () => print('answer 2 chosen'),
+                child: Text('Answer 2')),
+            ElevatedButton(
+                onPressed: () {
+                  print('answer 3 chosen');
+                },
+                child: Text('Answer 3'))
+          ],
+        ),
       ),
     );
   }
